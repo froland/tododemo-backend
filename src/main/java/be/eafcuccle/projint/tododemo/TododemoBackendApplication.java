edbf9a904f2ad7b197161c55022da81d951d450c
Francois Roland <froland@froland.be>
2023-06-05 11:13:36 +0200

    Create project.

---
+++ src/main/java/be/eafcuccle/projint/tododemo/TododemoBackendApplication.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.tododemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TododemoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TododemoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/eafcuccle/projint/tododemo/TododemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.tododemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TododemoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
