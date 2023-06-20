ca9b0f45fae484183544ff9388feddaa9376fe8b
Francois Roland <froland@froland.be>
2023-06-20 16:20:30 +0200

    Guard endpoints with authorizations.

--- src/main/java/be/eafcuccle/projint/tododemo/TodoApiController.java
+++ src/main/java/be/eafcuccle/projint/tododemo/TodoApiController.java
@@ -1,5 +1,6 @@
 package be.eafcuccle.projint.tododemo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -22,36 +23,53 @@ public class TodoApiController {
 
   @GetMapping
   public ResponseEntity<List<Todo>> getAllTodos(Authentication authentication) {
-    TodoUser owner = getOwnerFromAuthentication(authentication);
-    return ResponseEntity.ok(todoRepository.findByOwner(owner));
+    if (hasAuthority(authentication, "SCOPE_read:all-todos")) {
+      return ResponseEntity.ok(todoRepository.findAll());
+    } else if (hasAuthority(authentication, "SCOPE_read:todos")) {
+      TodoUser owner = getOwnerFromAuthentication(authentication);
+      return ResponseEntity.ok(todoRepository.findByOwner(owner));
+    } else {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<Todo> getTodoById(@PathVariable UUID id, Authentication authentication) {
-    TodoUser owner = getOwnerFromAuthentication(authentication);
-    Todo foundTodo = todoRepository.findByIdAndOwner(id, owner).orElseThrow();
-    return ResponseEntity.ok(foundTodo);
+    if (hasAuthority(authentication, "SCOPE_read:all-todos") || hasAuthority(authentication, "SCOPE_read:todos")) {
+      TodoUser owner = getOwnerFromAuthentication(authentication);
+      return todoRepository.findByIdAndOwner(id, owner).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    } else {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
   }
 
   @PostMapping
   public ResponseEntity<Todo> createNewTodo(@RequestBody Todo todo, UriComponentsBuilder uriBuilder, Authentication authentication) {
-    TodoUser owner = getOwnerFromAuthentication(authentication);
-    Todo savedTodo = todoRepository.save(new Todo(todo.getDescription(), owner));
-    URI todoUri = uriBuilder.pathSegment("api", "todos", "{id}").build(savedTodo.getId());
-    return ResponseEntity.created(todoUri).body(savedTodo);
+    if (hasAuthority(authentication, "SCOPE_write:todos")) {
+      TodoUser owner = getOwnerFromAuthentication(authentication);
+      Todo savedTodo = todoRepository.save(new Todo(todo.getDescription(), owner));
+      URI todoUri = uriBuilder.pathSegment("api", "todos", "{id}").build(savedTodo.getId());
+      return ResponseEntity.created(todoUri).body(savedTodo);
+    } else {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<Todo> updateTodoById(@PathVariable UUID id, @RequestBody Todo todo, Authentication authentication) {
-    if (!id.equals(todo.getId())) {
-      return ResponseEntity.badRequest().build();
+    if (hasAuthority(authentication, "SCOPE_write:todos")) {
+      if (!id.equals(todo.getId())) {
+        return ResponseEntity.badRequest().build();
+      }
+      TodoUser owner = getOwnerFromAuthentication(authentication);
+      Todo foundTodo = todoRepository.findByIdAndOwner(id, owner).orElseThrow();
+      foundTodo.setDescription(todo.getDescription());
+      foundTodo.setDone(todo.getDone());
+      todoRepository.save(foundTodo);
+      return ResponseEntity.noContent().build();
+    } else {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
-    TodoUser owner = getOwnerFromAuthentication(authentication);
-    Todo foundTodo = todoRepository.findByIdAndOwner(id, owner).orElseThrow();
-    foundTodo.setDescription(todo.getDescription());
-    foundTodo.setDone(todo.getDone());
-    todoRepository.save(foundTodo);
-    return ResponseEntity.ok(foundTodo);
   }
 
   private TodoUser getOwnerFromAuthentication(Authentication authentication) {
@@ -61,4 +79,9 @@ public class TodoApiController {
       return todoUserRepository.save(newUser);
     });
   }
+
+  private static boolean hasAuthority(Authentication authentication, String expectedAuthority) {
+    return authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals(expectedAuthority));
+  }
+
 }
